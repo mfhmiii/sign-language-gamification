86c630c997507263c5e9ac09a2f5e0982079fabc
Muhammad Fahmi Zuhdi <fahmizhd52@gmail.com>
2025-06-05 06:12:42 +0700

    bug fixing - 3

--- app/(features)/quiz/[levelId]/[stageId]/page.tsx
+++ app/(features)/quiz/[levelId]/[stageId]/page.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from "react";
 import QuizPageClient from "./QuizPageClient";
+import { use } from "react";
 
 interface PageProps {
-  params: { levelId: string; stageId: string };
+  params: Promise<{ levelId: string; stageId: string }> | { levelId: string; stageId: string };
 }
 
-export default async function QuizPage({ params }: PageProps) {
-  const resolvedParams = await params;
+export default function QuizPage({ params }: PageProps) {
+  const resolvedParams = use(Promise.resolve(params));
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <QuizPageClient
